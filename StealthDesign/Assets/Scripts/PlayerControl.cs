4d02aa5b95b7181125b6c184ea3f6ae9f3c14fb4
D19127919 <77803253+D19127919@users.noreply.github.com>
2022-02-24 09:38:04 +0000

    Changes
    Increased the size of the player's hitbox for picking stuff up
    Crouch is now C instead of Ctrl
    Player now runs at roughly the previous walking speed by holding shift, with a stamina system.

--- StealthDesign/Assets/Scripts/PlayerControl.cs
+++ StealthDesign/Assets/Scripts/PlayerControl.cs
@@ -10,6 +10,10 @@ public class PlayerControl : MonoBehaviour
     public static Vector3 playerPos;
     public static bool crouched = false;
     public float crouchSpeedMult = 0.5f;
+
+    public float maxStamina = 5;
+    private float stamina;
+    public float runSpeedMult = 2;
     
     private Vector3 moveDirection; //The direction the player is moving in
     private Rigidbody myRB; //The player's rigidbody
@@ -22,6 +26,7 @@ public class PlayerControl : MonoBehaviour
     void Start()
     {
         myRB = GetComponent<Rigidbody>();
+        stamina = maxStamina;
 
         //Getting player position
         StartCoroutine(TrackPlayer());
@@ -47,7 +52,13 @@ public class PlayerControl : MonoBehaviour
         Vector3 movementVelocity = moveDirection * movespeed; //Multiply by speed to get velocity
         if(crouched)
         {
-            movementVelocity *= crouchSpeedMult;
+            movementVelocity *= crouchSpeedMult; //If you're crouching, multiply your speed by the relevant multiplier.
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) && !crouched && stamina > 0)//If you're sprinting and not crouching and have stamina...
+        {
+            movementVelocity *= runSpeedMult; //Multiply your speed by the relevant multiplier.
+            stamina -= Time.deltaTime; //Reduce your stamina.
         }
 
         myRB.velocity = movementVelocity; //Apply that to your rigidbody
@@ -57,16 +68,20 @@ public class PlayerControl : MonoBehaviour
             myRB.AddForce(0, -movespeed * extraGravity, 0);
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKeyDown(KeyCode.C))
         {
             gameObject.transform.localScale /= 2;
             crouched = true;
         }
-
-        if (Input.GetKeyUp(KeyCode.LeftControl))
+        if (Input.GetKeyUp(KeyCode.C))
         {
             gameObject.transform.localScale *= 2;
             crouched = false;
+        }
+
+        if (!Input.GetKey(KeyCode.LeftShift) && stamina < maxStamina)
+        {
+            stamina += Time.deltaTime;
         }
     }
 
